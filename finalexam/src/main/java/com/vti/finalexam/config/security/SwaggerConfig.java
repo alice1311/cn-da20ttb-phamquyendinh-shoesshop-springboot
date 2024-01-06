f047181287c6a88f6d480b4c6fab27f0f498988c
alice1311 <pdinh0823@gmail.com>
2024-01-06 10:31:38 +0700

    set up swagger

---
+++ finalexam/src/main/java/com/vti/finalexam/config/security/SwaggerConfig.java
@@ -0,0 +1,63 @@
+package com.vti.finalexam.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
+import java.util.Set;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    private ApiKey apiKey(){
+        return new ApiKey("Authorization", "Authorization", "header");
+
+    }
+    private SecurityContext securityContext(){
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return List.of(new SecurityReference("Authorization", authorizationScopes));
+    }
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.OAS_30)
+                .pathMapping("/")
+                .enable(true)
+                .securityContexts(List.of(securityContext()))
+                .securitySchemes(List.of(apiKey()))
+                .apiInfo(apiInfo())
+                .select()
+                    .apis(RequestHandlerSelectors.any())
+                    .paths(PathSelectors.any())
+                    .build()
+                .groupName("finalexam")
+                .protocols(Set.of("https", "http"));
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .version("u.0.c")
+                .title("Api Final Exam")
+                .description("demo swagger for final exam")
+                .contact(new Contact("Dinh", null, "pdinh0823@gmail.com"))
+                .termsOfServiceUrl("http://localhost:8080/").termsOfServiceUrl("http://192.168.88.210:8080")
+                .license("Apache 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0.html")
+                .build();
+    }
+
+
+}
